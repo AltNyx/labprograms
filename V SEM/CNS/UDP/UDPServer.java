e312710a0cf58a39c147ac5bae59530cae452863
piyx <piyx.abhishek@gmail.com>
2021-02-02 01:51:00 +0530

    formatting
    Former-commit-id: cc6a9ba9c67a29eb58385545f894e88eef86c8a1 [formerly 666ac3fca8361b00889163bf48df977409611b13]
    Former-commit-id: 0ea5e3c0c351e2d30d299ce1bfd2680dcf1db070
    Former-commit-id: 72951fa01df16ced7ec6b7f62a690e221062a0a6 [formerly 7f6e35e4f9a402bff83e8ded5844b402ba98f386]
    Former-commit-id: 26281ff6a0f9cf4ac9fb4648260dab43fda15578

--- V SEM/CNS/UDP/UDPServer.java
+++ V SEM/CNS/UDP/UDPServer.java
@@ -3,26 +3,27 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
-
 public class UDPServer {
     public static void main(String[] args) throws Exception {
         DatagramSocket ds = new DatagramSocket();
-        
+
         InetAddress ip = InetAddress.getByName("localhost");
         int port = 7000;
-        
+
         boolean hasQuit = false;
         Scanner sc = new Scanner(System.in);
         DatagramPacket dp;
-        
+
         // Read message and send it to client until server quits
         while (!hasQuit) {
             String msg = sc.nextLine();
-            if (msg.equals("quit")) { hasQuit = true; }
+            if (msg.equals("quit")) {
+                hasQuit = true;
+            }
             dp = new DatagramPacket(msg.getBytes(), msg.length(), ip, port);
             ds.send(dp);
         }
-        
+
         sc.close();
         ds.close();
     }
