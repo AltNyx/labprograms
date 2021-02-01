4e37a502d9b23686843d6ce280ee676c53ae5cc7
piyx <piyx.abhishek@gmail.com>
2021-02-01 22:47:10 +0530

    update prog3 (ethernet lan)
    Former-commit-id: be1ba1e24509ade7babe5d556aa3ea2cc7640d54 [formerly 9fea2b8460dc94c1d8692c6b2bf6d00ede388186]
    Former-commit-id: cdcf95709f0cdc7965cdbd59162894857f6f6475
    Former-commit-id: 4ba5ae447b7af023b7a2adac64558b3e07984e49 [formerly baa1b4a16b714156daa49c35008bdb0cd23543c2]
    Former-commit-id: d3c37bfeaecedc01c7975ec2b97eef2ef4f24a38

--- V SEM/CNS/ETHERNETLAN/prog3.tcl
+++ V SEM/CNS/ETHERNETLAN/prog3.tcl
@@ -1,12 +1,11 @@
 set ns [new Simulator]
 
-set tracefile [open prog3.tr w]
-$ns trace-all $tracefile
+set tf [open 3.tr w]
+$ns trace-all $tf
 
-set namfile [open prog3.nam w]
-$ns namtrace-all $namfile
+set nf [open 3.nam w]
+$ns namtrace-all $nf
 
-# Create nodes
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
@@ -14,17 +13,10 @@ set n3 [$ns node]
 
 $ns make-lan "$n0 $n1 $n2 $n3" 10Mb 10ms LL Queue/DropTail Mac/802_3
 
-$ns color 1 "red"
-$ns color 2 "blue"
-
-# Attach agents
 set tcp0 [new Agent/TCP]
 $ns attach-agent $n0 $tcp0
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
-
-set sink3 [new Agent/TCPSink]
-$ns attach-agent $n3 $sink3
 
 set tcp2 [new Agent/TCP]
 $ns attach-agent $n2 $tcp2
@@ -34,42 +26,45 @@ $ftp2 attach-agent $tcp2
 set sink1 [new Agent/TCPSink]
 $ns attach-agent $n1 $sink1
 
-# Connect the agents
+set sink3 [new Agent/TCPSink]
+$ns attach-agent $n3 $sink3
+
 $ns connect $tcp0 $sink3
 $ns connect $tcp2 $sink1
 
-set file2 [open file2.tr w]
-$tcp2 attach $file2
-$tcp2 trace cwnd_
+$ns color 1 red
+$ns color 2 blue
 
 $tcp0 set class_ 1
 $tcp2 set class_ 2
 
-set file1 [open file1.tr w]
-$tcp0 attach $file1
+set file0 [open file0.tr w]
+$tcp0 attach $file0
 $tcp0 trace cwnd_
-$tcp0 set maxcwnd_ 10
+
+set file2 [open file2.tr w]
+$tcp2 attach $file2
+$tcp2 trace cwnd_
+$tcp2 set maxcwnd_ 10
 
 proc finish {} {
-    global ns namfile tracefile
+    global ns nf tf
     $ns flush-trace
-    close $tracefile
-    close $namfile
-    exec nam prog3.nam &
+    exec nam 3.nam &
+    close $tf
+    close $nf
     exit 0
 }
 
 $ns at 0.1 "$ftp0 start"
 $ns at 1.5 "$ftp0 stop"
-
-$ns at 2 "$ftp0 start"
-$ns at 3 "$ftp0 stop"
+$ns at 2.0 "$ftp0 start"
+$ns at 4.0 "$ftp0 stop"
 
 $ns at 0.2 "$ftp2 start"
-$ns at 2 "$ftp2 stop"
-
+$ns at 2.0 "$ftp2 stop"
 $ns at 2.5 "$ftp2 start"
-$ns at 4 "$ftp2 stop"
+$ns at 4.5 "$ftp2 stop"
 
 $ns at 5.0 "finish"
 $ns run
